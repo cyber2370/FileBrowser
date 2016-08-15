a06978dc50b4650bb9761c2f626bb2f7c71fa606
Borys <Borys@DESKTOP-QVVCHQC>
2016-08-15 23:44:13 +0300

    Deleted standart code from Home/Index html, add controller

---
+++ WebFileBrowser/WebFileBrowser/Controllers/NavigationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebFileBrowser.Models;
+
+namespace WebFileBrowser.Controllers
+{
+    public class NavigationController : ApiController
+    {
+        public IHttpActionResult GetProduct(int id)
+        {
+            var directory = new DirectoryModel
+            {
+                
+            };
+
+            if (directory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(directory);
+        }
+    }
+}
---
+++ WebFileBrowser/WebFileBrowser/Models/DirectoryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFileBrowser.Models
+{
+    public class DirectoryModel
+    {
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+
+        public IList<FileModel> Files { get; set; }
+
+        public IList<DirectoryModel> Directories { get; set; }
+
+    }
+}
